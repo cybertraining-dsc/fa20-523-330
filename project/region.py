57db9555142d7a7f8b944f86352b6794239894b9
s0000011 <20000479+s0000011@users.noreply.github.com>
2020-10-20 14:53:39 -0400

    Update region.py

--- project/region.py
+++ project/region.py
@@ -5,9 +5,6 @@ Created on Mon Oct 19 14:33:58 2020
 
 @author: tobi
 """
-##conda install -c conda-forge folium
-##pip install selenium
-
 import pandas as pd
 df = pd.read_csv('olist_customers_dataset.csv')
 hist = df.hist
